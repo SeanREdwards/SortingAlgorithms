4ba3927c5e035d8b008cff986a2e8fbb700426fb
SeanREdwards <edwards.897@buckeyemail.osu.edu>
2021-07-17 16:54:04 -0400

    Comments added to QuickSort.js

--- JavaScript/src/QuickSort.js
+++ JavaScript/src/QuickSort.js
@@ -1,27 +1,51 @@
+/*
+ *QuickSort.js
+ *A JavaScript implementation of the QuickSort algorithm (O(n^2)). Conceptually Quicksort is a divide and conquer algorithm that
+ *partitions into sub-arrays around a pivot element, and then sorts the sub arrays with recursive calls.
+ * 
+ *@Author Sean Edwards
+ *@Date 7/17/2021
+ */
+
+/*
+*Implementation of QuickSort based upon the version discussed in "Introduction to Algorithms 3rd Edition"  by Cormen, Lesiserson, Rivest, & Stein pg 170.
+*/
 export function quickSort(arr, firstIndex, lastIndex){
-    
+
     if (firstIndex < lastIndex){
+
+        //Obtain the partition index where a pivot element has previously been relocated to
         let partitionIndex = partition(arr, firstIndex, lastIndex);
 
+        //Recursive calls to sort sub-arrays
         quickSort(arr, firstIndex, partitionIndex - 1);
         quickSort(arr, partitionIndex + 1, lastIndex);
     }
 }
 
+/*
+*Partitions the array into sub-arrays by utilizing a pivot element determine which partition to 
+*place a compared element.
+*/
 export function partition(arr, firstIndex, lastIndex){
     let pivotElement = arr[lastIndex];
     let i = firstIndex - 1;
 
+    //Rearrange the array based on the pivot element.
     for(let j = firstIndex; j < lastIndex; j++){
+
         if(arr[j] <= pivotElement){
+
             i++;
             swap(arr, i, j);
         }
     }
+
     swap(arr, i+1, lastIndex);
     return (i+1);
 }
 
+//Simple swap function to exchange two elements in an array
 export function swap(arr, i, j){
     let swapVal = arr[i];
     arr[i] = arr[j];
